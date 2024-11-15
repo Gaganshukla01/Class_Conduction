cb43fa735e030b4f8956e0bc096d12308c43a920
Gagan <gaganshuk@gmail.com>
2024-11-15 19:54:35 +0530

    temp

--- server/server.js
+++ server/server.js
@@ -1,17 +1,35 @@
-const express=require("express")
-
-const app=express()
-const port =process.env.PORT||3000
-
-app.get('/',(req,res)=>{
-res.send("Hello i am here")
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
 
 
-})
 
 
-app.listen(port,()=>{
-    console.log(`Server is running .. on that link  http://localhost:${port}/#`)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-})
+
+
+app.get("/api/data", (req, res) => {
+  res.json({
+    message: "This is some sample data.",
+    success: true,
+  });
+});
+
+app.post("/api/data", (req, res) => {
+  const data = req.body;
+  res.json({
+    message: "Data received successfully!",
+    data: data,
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
