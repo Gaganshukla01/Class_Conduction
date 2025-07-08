2aac7d0831122ee90adb082671702610a453a82c
Gaganshukla <gaganshukla@gmail.com>
2025-07-08 12:13:50 +0530

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,32 @@
-import React from 'react'
-import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import './App.css'
-import ResetPassword from './pages/ResetPassword'
-import EmailVerify from './pages/EmailVerify'
-import AdminDashboard from './pages/AdminPannel'
-import StudentDashboard from './pages/StudentSection'
-import { ToastContainer } from 'react-toastify'
-import Navbar from './components/Navbar'
+import React from "react";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import "./App.css";
+import ResetPassword from "./pages/ResetPassword";
+import EmailVerify from "./pages/EmailVerify";
+import AdminDashboard from "./pages/AdminPannel";
+import StudentDashboard from "./pages/StudentSection";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <ToastContainer />
-    
-     
+
       <div id="main-flow">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/resetPassword' element={<ResetPassword />} />
-          <Route path='/emailVerify' element={<EmailVerify />} />
-          <Route path='/admin' element={<AdminDashboard />} />
-          <Route path='/studentdash' element={<StudentDashboard />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/resetPassword" element={<ResetPassword />} />
+          <Route path="/emailVerify" element={<EmailVerify />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/studentdash" element={<StudentDashboard />} />
         </Routes>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/context/Context.jsx
+++ client/src/context/Context.jsx
@@ -1,87 +1,78 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
-const backend_url=import.meta.env.VITE_BACKEND_URI
-export const AppContent=createContext()
+const backend_url = import.meta.env.VITE_BACKEND_URI;
+export const AppContent = createContext();
 
+export const AppContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedin] = useState(false);
+  const [userData, setUserData] = useState(false);
+  const [allUserData, setAllUserData] = useState(false);
+  const [allCourse, setAllCourseData] = useState(false);
 
-export const  AppContextProvider=(props)=>{
-
-   
-    const[isLoggedIn,setIsLoggedin]=useState(false)
-    const[userData,setUserData]=useState(false)
-    const[allUserData,setAllUserData]=useState(false)
-    const[allCourse,setAllCourseData]=useState(false)
-
-    const getAuthState=async()=>{
-            axios.defaults.withCredentials=true
-            try {
-                const {data} = await axios.get(`${backend_url}/api/auth/isAuthenticate`);
-                if (data.sucess) {
-                    setIsLoggedin(true);
-                    getUserData();
-                    getAllUserData();
-                    getAllCourse()
-                }
-            } catch (error) {
-                console.log(error.response?.data?.message || "An error occurred");
-            }
-            
-    
+  const getAuthState = async () => {
+    axios.defaults.withCredentials = true;
+    try {
+      const { data } = await axios.get(
+        `${backend_url}/api/auth/isAuthenticate`
+      );
+      if (data.sucess) {
+        setIsLoggedin(true);
+        getUserData();
+        getAllUserData();
+        getAllCourse();
+      }
+    } catch (error) {
+      console.log(error.response?.data?.message || "An error occurred");
     }
+  };
 
-    const getUserData=async()=>{
-        try {
-            const {data}= await axios.get(`${backend_url}/api/user/data`)
-            data.sucess?setUserData(data.message):toast.error(data.message)
-            
-        } catch (error) {
-            toast.error(data.message)
-        }
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(`${backend_url}/api/user/data`);
+      data.sucess ? setUserData(data.message) : toast.error(data.message);
+    } catch (error) {
+      toast.error(data.message);
     }
+  };
 
-    useEffect(()=>{
-        getAuthState()
-    
-    },[])
+  useEffect(() => {
+    getAuthState();
+  }, []);
 
-        const getAllUserData=async()=>{
-           axios.defaults.withCredentials=true
-          try {
-            const {data}= await axios.get(`${backend_url}/api/user/getalluser`)
-            data.success?setAllUserData(data):toast.error(data.message)
-        } catch (error) {
-            toast.error(data.message)
-        }
-
+  const getAllUserData = async () => {
+    axios.defaults.withCredentials = true;
+    try {
+      const { data } = await axios.get(`${backend_url}/api/user/getalluser`);
+      data.success ? setAllUserData(data) : toast.error(data.message);
+    } catch (error) {
+      toast.error(data.message);
     }
-        const getAllCourse=async()=>{
-           axios.defaults.withCredentials=true
-          try {
-            const res= await axios.get(`${backend_url}/api/course/`)
-            res.data?setAllCourseData(res.data):toast.error(res.message)
-        } catch (error) {
-            toast.error(data.message)
-        }
-
+  };
+  const getAllCourse = async () => {
+    axios.defaults.withCredentials = true;
+    try {
+      const res = await axios.get(`${backend_url}/api/course/`);
+      res.data ? setAllCourseData(res.data) : toast.error(res.message);
+    } catch (error) {
+      toast.error(data.message);
     }
+  };
 
-    const value={
-        backend_url,
-        isLoggedIn,setIsLoggedin,
-        userData,setUserData,
-        getUserData,
-        getAllUserData,
-        allUserData,
-        getAllCourse,
-        allCourse
+  const value = {
+    backend_url,
+    isLoggedIn,
+    setIsLoggedin,
+    userData,
+    setUserData,
+    getUserData,
+    getAllUserData,
+    allUserData,
+    getAllCourse,
+    allCourse,
+  };
 
-
-    }
-
-    return (
-        <AppContent.Provider value={value}>
-            {props.children}
-        </AppContent.Provider>
-    )
-}
+  return (
+    <AppContent.Provider value={value}>{props.children}</AppContent.Provider>
+  );
+};
